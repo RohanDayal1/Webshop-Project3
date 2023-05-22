ab991a5bfd0cc1561e17d9ba4b92f3a056c4af7e
RohanD <113175563+RohanDayal1@users.noreply.github.com>
2023-05-22 14:21:56 +0200

    Add files via upload

---
+++ Homepage/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("../Library/head.html"); ?>
+<body>
+    <?php include("../Library/menu.php"); ?>
+    <!-- Page Code Goes Here --> 
+    <main id="Homepage_Main">
+        <!-- <a href="../Systeembeheerder/gemiddeldebedrag.php">click</a>  -->
+        <div id="Homepage_Logo_Trapezoid">
+            <p>De Lezers</p>
+        </div>
+    </main>
+    <!-- Temp until menu gets done -->
+
+    <?php include("../Library/footer.html"); ?>
+</body>
+</html>
---
+++ Medewerkerspage/Medewerkers.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("../Library/head.html"); ?>
+
+<body class="medbody">
+
+<?php include("../Library/menu.php"); ?>
+
+    <main id="med_main">
+        <div class="med-flex-container">
+            <div><img class="medpimg" src="../img/ShondriqueFoto.jpg" alt="Foto van Shondrique"><h2 class="medh2">Shondrique</h2> <p class="medpinfo">Hallo, ik ben Shondrique en ben 18 jaar. Als opleiding doe ik Software Developer op de Techniek College. Als u graag van lezen houdt bent u op de goede plek.</p></div>
+            <div><img class="medpimg" src="../img/emilypic.jpeg" alt="Foto van Emily"><h2 class="medh2">Emily</h2><p class="medpinfo">Hallo! Mijn naam is Emily Broeksema. Ik ben een eerste jaars Software Developer op Techniek College Rotterdam. De lezers is een groeps project gemaakt voor mensen die houden van lezen.</p></div>
+            <div><img class="medpimg" src="../img/SofiePortfolio.jpg" alt="Foto van Sofie"><h2 class="medh2">Sofie</h2><p class="medpinfo">Hoi, Ik ben Sofie! Ik ben 18 jaar en volg momenteel de opleiding Software Developer op het Techniek College Rotterdam.Hiervoor heb ik gekozen omdat het iets wat ik erg leuk vind om te doen in mijn vrije tijd.</p></div>
+            <div><img class="medpimg" src="../img/RohanPortfolio.jpg" alt="Foto van Rohan"><h2 class="medh2">Rohan</h2><p class="medpinfo">Hallo, mijn naam is Rohan Dayal. Ik ben nu een student Software Developer op Techniek College Rotterdam. ik heb voor deze opleiding gekozen, omdat ik technologie altijd al fascinered vond.</p></div>  
+        </div>
+
+        
+        
+        
+        <h1 class= "medh1">Ons Bedrijf</h1>
+        
+        <div class="meddiv">
+            <div class="medpdiv">
+                <p class= "medp">Onze bedrijf De Lezers verkoopt boeken voor gebruikers. Deze boeke zijn speciaal voor Lezers, 
+        dikke boeken met wel 200 pagina's. Onze website heeft een makkelijk te navigeren interface met 
+        zoekfilters om de gebruikers te helpen biedt ook informatie over elk boek, de bladzijden, de 
+        samenvatting en meer. Gebruikers kunnen boeken kopen via de website  </p>
+            </div>
+        
+
+        <img class="medimg" src="./bedrijf.png" alt="bedrijf">
+
+        </div>
+        
+
+    </main>
+
+    
+
+    <?php include("../Library/footer.html") ?>
+</body>
+</html>
